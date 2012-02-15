b1cc8e0a98d8a7eeda0fd7d3998e7511cea4a831
YoungUk Kim <bluekyu.dev@gmail.com>
2012-02-15 14:37:36 +0900

    패키지 구조 변경

--- setup.py
+++ setup.py
@@ -4,5 +4,12 @@ from distutils.core import setup
 
 setup(name='kyuNotebook',
       version='0.1.0',
+      description='Note taking program',
+      author='YoungUk Kim',
+      author_email='bluekyu.dev@gmail.com',
+      url='http://www.bluekyu.me/',
       packages=['kyuNotebook'],
+      package_dir={'': 'src'},
+      scripts=['src/kyuNotebook.pyw'],
+      license='GNU GPL v3'
       )
