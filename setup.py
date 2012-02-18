d19a08c60bdfb6e7eff14d0055c0114b5332d537
YoungUk Kim <bluekyu.dev@gmail.com>
2012-02-18 16:31:39 +0900

    리눅스 데스크톱 아이콘 추가

--- setup.py
+++ setup.py
@@ -10,7 +10,10 @@ if srcDirPath not in sys.path:
 import kyunotebooklib
 
 desktop_files = [
-        ('share/icons/hicolor/32x32/apps', ['desktop/kyuNotebook.png']),
+        ('share/icons/hicolor/48x48/apps', ['desktop/48x48/kyuNotebook.png']),
+        ('share/icons/hicolor/32x32/apps', ['desktop/32x32/kyuNotebook.png']),
+        ('share/icons/hicolor/24x24/apps', ['desktop/24x24/kyuNotebook.png']),
+        ('share/icons/hicolor/16x16/apps', ['desktop/16x16/kyuNotebook.png']),
         ('share/applications', ['desktop/kyuNotebook.desktop'])]
 
 if sys.platform.startswith('linux'):
